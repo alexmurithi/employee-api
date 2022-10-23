b95083ad877783650e8194812ce18c051a5bda3f
Alex Gitari Murithi <gitarimurithialex@gmail.com>
2022-10-23 19:58:26 +0300

    cors

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/api/v1")
 public class EmployeeController {
     @Autowired
