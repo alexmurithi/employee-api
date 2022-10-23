e80b5381b1cd459a9666536f61b61cf034691c47
Alex Gitari Murithi <gitarimurithialex@gmail.com>
2022-10-23 13:53:24 +0300

    get employee by id api

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.employee.controller;
 
+import com.employee.exception.ResourceNotFoundException;
 import com.employee.model.Employee;
 import com.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,13 @@ public class EmployeeController {
     @PostMapping(path = "/employees")
     public Employee addEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    //GET EMPLOYEE BY ID
+    @GetMapping(path = "employees/{id}")
+    public ResponseEntity <Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Employee does not exist with id: "+ id));
+        return ResponseEntity.ok(employee);
     }
 }
--- src/main/java/com/employee/model/Employee.java
+++ src/main/java/com/employee/model/Employee.java
@@ -20,7 +20,7 @@ public class Employee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
+    private Long id;
 
     @Column(name = "firstName")
     private String firstName;
